27f31a779833db9eee1a42e5db6cfe96c3249394
Lukas <lukas@infornautik.de>
2020-11-22 23:45:37 +0100

    fix: Compiler issues

--- src/app/device-module/auth.guard.ts
+++ src/app/device-module/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
-import { tap } from 'rxjs/internal/operators/tap';
+import { tap } from 'rxjs/operators';
 import { AuthService } from '../common-module/_services';
 
 @Injectable({ providedIn: 'root' })
