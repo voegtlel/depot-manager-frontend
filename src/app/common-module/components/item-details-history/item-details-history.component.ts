6a5535710eef91d3601f716abf1cba4b40689739
Lukas <lukas@infornautik.de>
2021-08-28 17:36:15 +0200

    feat: Split reports and item history

--- src/app/common-module/components/item-details-history/item-details-history.component.ts
+++ src/app/common-module/components/item-details-history/item-details-history.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { ItemState } from '../../_models';
 import { ApiService } from '../../_services';
 
@@ -16,10 +16,26 @@ interface ItemStateWithArray extends ItemState {
     templateUrl: './item-details-history.component.html',
     styleUrls: ['./item-details-history.component.scss'],
 })
-export class ItemDetailsHistoryComponent {
+export class ItemDetailsHistoryComponent implements OnChanges {
     @Input() states: ItemStateWithArray[];
+    @Input() noReports = false;
+    @Input() onlyReports = false;
+
+    displayStates: ItemStateWithArray[];
 
     constructor(private api: ApiService) {}
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (this.states) {
+            if (this.noReports) {
+                this.displayStates = this.states.filter((state) => state.changesArray.length > 0);
+            } else if (this.onlyReports) {
+                this.displayStates = this.states.filter((state) => state.report != null);
+            } else {
+                this.displayStates = this.states;
+            }
+        }
+    }
 
     getPicturePreviewUrl(pictureId: string): string {
         return this.api.getPicturePreviewUrl(pictureId);
